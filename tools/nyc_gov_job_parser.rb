816d43d516f6803771c69faf4c50ee8083296569
Ephraim Friedman & Jonathan Marchand <pair+ephraimfriedman.jmarsh433@devbootcamp.com>
2016-08-19 17:13:49 -0400

    done

--- tools/nyc_gov_job_parser.rb
+++ tools/nyc_gov_job_parser.rb
@@ -4,6 +4,10 @@ require 'json'
 module NYCGovJobParser
   BASE_URL = "https://data.cityofnewyork.us/resource/swhp-yxa4.json"
 
+  BASE_QUERY_FOR_SALARY_MIN = "?$where=salary_range_from > "
+
+  BASE_QUERY_FOR_AGENCY = "?$where=agency="
+
   def self.all_listings
     json_string_response = open(BASE_URL).read
 
@@ -24,5 +28,36 @@ module NYCGovJobParser
     end
     agencies_array.sort
   end
+
+  def self.min_salary(amount)
+    json_string_response = open(BASE_URL + BASE_QUERY_FOR_SALARY_MIN + "\"" + amount +"\"").read
+
+    JSON.parse(json_string_response)
+  end
+
+  def self.find_agency(agency)
+    json_string_response = open(BASE_URL + BASE_QUERY_FOR_AGENCY + "\"" + agency +"\"").read
+
+    JSON.parse(json_string_response)
+    BASE_URL + BASE_QUERY_FOR_AGENCY + "\"" + agency +"\""
+
+  end
+
+  def self.parse_with_agency_and_salary(amount, agency)
+    json_string_response = open(BASE_URL + BASE_QUERY_FOR_AGENCY + "\"" + agency + "\"" + "AND salary_range_from > " + "\"" + amount +"\"" ).read
+
+    JSON.parse(json_string_response)
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
 
