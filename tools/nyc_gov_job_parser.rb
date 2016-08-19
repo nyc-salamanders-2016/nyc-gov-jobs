a86f8ab0a3bcee2a2cfb1008bdaf3b4fc1612bbe
Henri El Daher & Kiren Srinivasan <pair+heldaher.tuneguru@devbootcamp.com>
2016-08-19 11:39:49 -0400

    Adds Parser module

---
+++ tools/nyc_gov_job_parser.rb
@@ -0,0 +1,17 @@
+require 'open-uri'
+require 'json'
+
+module NYCGovJobParser
+  BASE_URL = "https://data.cityofnewyork.us/resource/swhp-yxa4.json"
+
+  def self.all_listings
+    json_string_response = open().read
+
+    # Return an array of hashes
+    JSON.parse(json_string_response)
+  end
+end
+
+module NYCGovJobParser
+
+end
